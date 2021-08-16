43df82cb878d7e405a4d6674aa34f87993499218
MohamedElmdary <engm5081@gmail.com>
2021-08-16 12:11:50 +0200

    Added register

--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -1,1 +1,16 @@
-<h1>Login works!</h1>
+<section class="login container">
+  <h1>Login works!</h1>
+
+  <form class="form">
+    <div class="form__field">
+      <input type="text" />
+    </div>
+    <div class="form__field">
+      <input type="text" />
+    </div>
+    <div class="form__field">
+      <input type="text" />
+    </div>
+    <button type="submit"> Login </button>
+  </form>
+</section>
--- src/pages/Register.svelte
+++ src/pages/Register.svelte
@@ -1,1 +1,69 @@
-<h1>Register works!</h1>
+<script lang="ts">
+  import { push } from "svelte-spa-router";
+  import http from "../configs/http";
+
+  const form = {
+    username: "",
+    email: "",
+    password: "",
+  };
+
+  function register() {
+    http
+      .post("/user", form)
+      .then(() => {
+        push("/login");
+      })
+      .catch(({ response }) => {
+        console.log(response.status, response.data);
+      });
+  }
+</script>
+
+<section class="register container">
+  <h1>Register</h1>
+
+  <form class="form" on:submit|preventDefault={register}>
+    <div class="form__field">
+      <input
+        bind:value={form.username}
+        type="text"
+        minlength="3"
+        placeholder="Username"
+        required
+      />
+    </div>
+
+    <div class="form__field">
+      <input
+        bind:value={form.email}
+        type="email"
+        placeholder="Email address"
+        required
+      />
+    </div>
+
+    <div class="form__field">
+      <input
+        bind:value={form.password}
+        type="password"
+        minlength="6"
+        placeholder="Password"
+        required
+      />
+    </div>
+    <button type="submit" class="btn"> Register </button>
+  </form>
+</section>
+
+<style lang="scss">
+  .register {
+    max-width: 500px;
+    margin: auto;
+    margin-top: 10rem;
+  }
+
+  .btn {
+    margin: auto;
+  }
+</style>
